79f23996a586fe2fd3ca66f50b534b4ebdfaaf60
Бажен Левковець <bazhen.paseka@gmail.com>
2020-04-13 12:12:26 +0300

    EEPROM added.

--- Drivers/Local_config/at24cXX_local_config.h
+++ Drivers/Local_config/at24cXX_local_config.h
@@ -8,6 +8,13 @@
 #ifndef LOCAL_CONFIG_AT24CXX_LOCAL_CONFIG_H_
 #define LOCAL_CONFIG_AT24CXX_LOCAL_CONFIG_H_
 
+/*
+**************************************************************************
+*								    DEFINES
+**************************************************************************
+*/
 
+	//#define EEPROM_DEVICE_ADDRESS 0b1010000 << 1
+	#define EEPROM_DEVICE_ADDRESS 0x50 << 1
 
 #endif /* LOCAL_CONFIG_AT24CXX_LOCAL_CONFIG_H_ */
